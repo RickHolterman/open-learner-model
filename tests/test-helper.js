e8e66e34ed0fa4e36d06eabccd3b0cbbdbb554b1
Rick Holterman <rick@pixelpillow.nl>
2022-09-05 15:42:17 +0200

    Init Ember app

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'open-learner-model/app';
+import config from 'open-learner-model/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
