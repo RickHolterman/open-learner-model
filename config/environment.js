e103d59271e1128a7006e040b6abc5de487f8ead
Rick Holterman <rick@pixelpillow.nl>
2023-10-01 19:24:47 +0200

    Set base url

--- config/environment.js
+++ config/environment.js
@@ -42,7 +42,7 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     // ENV.rootURL = 'https://rickholterman.github.io/open-learner-model/';
-    // ENV.baseURL = '/open-learner-model';
+    ENV.baseURL = '/open-learner-model';
     ENV.locationType = 'history';
     ENV['ember-cli-mirage'] = {
       enabled: true,
